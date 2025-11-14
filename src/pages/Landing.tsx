2efc691c590d352da302236baf26916f51fffb96
Alejandro Lopez <lopez.alejandro@dali.dartmouth.edu>
2025-11-13 19:33:16 -0500

    landing page fade in animations

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -8,6 +8,11 @@ import landingPattern from '../assets/landingPattern.svg';
 import { scrollToSection } from "../utils/constants";
 
 export default function Landing() {
+  const animationDelay: number = 0.5;
+  const animationDuration: number = 0.6;
+  const animationXStart: number = -10;
+  const animationYStart: number = -10;
+
   return (
     <div className="flex flex-col max-w-screen">
       <Header />
@@ -21,18 +26,18 @@ export default function Landing() {
       >
         <div className="flex flex-col self-end gap-6 max-w-8/12 sm: text-lightGrayText">
           <motion.p
-            initial={{ opacity: 0, y: -20, x: -20, scale: 0.98 }}
+            initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
             whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
-            transition={{ duration: 0.7, ease: "linear", delay: 0.7 * 0 }}
+            transition={{ duration: animationDuration, ease: "linear", delay: animationDelay * 0 }}
             viewport={{ once: true, amount: 0.2 }}
             className=" mt-48 text-4xl md:text-6xl xl:text-7xl font-medium text-whiteText"
           >
             Alex Lopez
           </motion.p>
           <motion.p
-            initial={{ opacity: 0, y: -20, x: -20, scale: 0.98 }}
+            initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
             whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
-            transition={{ duration: 0.7, ease: "linear", delay: 0.7 * 1 }}
+            transition={{ duration: animationDuration, ease: "linear", delay: animationDelay * 1 }}
             viewport={{ once: true, amount: 0.2 }}
             className="text-xl md:text-2xl lg:text-3xl"
           >
@@ -40,9 +45,9 @@ export default function Landing() {
             Automations and Data-Driven Systems
           </motion.p>
           <motion.p
-            initial={{ opacity: 0, y: -20, x: -20, scale: 0.98 }}
+            initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
             whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
-            transition={{ duration: 0.7, ease: "linear", delay: 0.7 * 2 }}
+            transition={{ duration: animationDuration, ease: "linear", delay: animationDelay * 2 }}
             viewport={{ once: true, amount: 0.2 }}
             className="text-md md:text-lg lg:text-xl"
           >
@@ -56,7 +61,7 @@ export default function Landing() {
             <motion.button
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.7, ease: "linear", delay: 0.7 * 3 }}
+              transition={{ duration: animationDuration, ease: "linear", delay: animationDelay * 3 }}
               viewport={{ once: true, amount: 0.2 }}
               className="border-2 bg-blackBg/30 hover:bg-blackBg/70 duration-300 transition border-primary text-primary text-lg md:text-xl 
               rounded-md h-20 w-full sm:w-72 cursor-pointer"
@@ -67,7 +72,7 @@ export default function Landing() {
             <motion.button
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.7, ease: "linear", delay: 0.7 * 3.2 }}
+              transition={{ duration: animationDuration, ease: "linear", delay: animationDelay * 3.2 }}
               viewport={{ once: true, amount: 0.2 }}
               className="border-2 bg-blackBg/30 hover:bg-blackBg/70 duration-300 transition border-primary text-primary text-lg md:text-xl 
               rounded-md h-20 w-full sm:w-72 cursor-pointer"
@@ -80,7 +85,7 @@ export default function Landing() {
         <motion.img
           initial={{ opacity: 0, scale: 0.98}}
           whileInView={{ opacity: 1, scale: 1 }} 
-          transition={{ duration: 0.7, ease: "easeIn", delay: 0.7 * 3.5 }}
+          transition={{ duration: animationDuration, ease: "easeIn", delay: animationDelay * 3.5 }}
           viewport={{ once: true, amount: 0.2 }}
           src={landingPattern}
           alt="landing pattern"
