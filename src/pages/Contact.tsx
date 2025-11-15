3ff7261aaefd932a7d933d811368605bb022dccb
Alejandro Lopez <lopez.alejandro@dali.dartmouth.edu>
2025-11-14 20:03:56 -0500

    contact page animation

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion, type Easing } from "framer-motion";
 import mailIcon from '../assets/mail.svg';
 import linkedinIcon from '../assets/linkedin.svg';
 import githubIcon from '../assets/github.svg';
@@ -7,6 +8,12 @@ export default function Contact() {
     const [name, setName] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [message, setMessage] = useState<string>("");
+
+    const animationDelay: number = 0.2;
+    const animationDuration: number = 0.4;
+    const animationXStart: number = 25;
+    const animationYStart: number = 0;
+    const animationEase: Easing = "easeOut";
 
     const contactLinks: { [key: string]: string } = {
         github: "https://github.com/AlejandroALopez",
@@ -28,13 +35,34 @@ export default function Contact() {
             <div className="flex flex-col gap-6 md:gap-12">
                 <div className="flex flex-col gap-4">
                     <div className="flex flex-col gap-3">
-                        <p className="font-medium text-3xl md:text-4xl">Let's work together</p>
-                        <p className="text-xl md:text-2xl">Have an idea that needs development? Let's talk!</p>
+                        <motion.p
+                            className="font-medium text-3xl md:text-4xl"
+                            initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
+                            whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+                            transition={{ duration: animationDuration, ease: animationEase, delay: animationDelay * 0 }}
+                            viewport={{ once: true, amount: 0.2 }}
+                        >Let's work together
+                        </motion.p>
+                        <motion.p
+                            className="text-xl md:text-2xl"
+                            initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
+                            whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+                            transition={{ duration: animationDuration, ease: animationEase, delay: animationDelay * 1 }}
+                            viewport={{ once: true, amount: 0.2 }}
+                        >
+                            Have an idea that needs development? Let's talk!
+                        </motion.p>
                     </div>
                 </div>
                 {/* Form fields */}
                 <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                    <div className="flex flex-col gap-1">
+                    <motion.div
+                        className="flex flex-col gap-1"
+                        initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
+                        whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+                        transition={{ duration: animationDuration, ease: animationEase, delay: animationDelay * 2 }}
+                        viewport={{ once: true, amount: 0.2 }}
+                    >
                         <p className="text-lg md:text-xl">Name</p>
                         <input
                             className="border sm:border-2 border-black rounded-sm p-2"
@@ -43,8 +71,14 @@ export default function Contact() {
                             value={name}
                             onChange={(e) => setName(e.target.value)}
                         />
-                    </div>
-                    <div className="flex flex-col gap-1">
+                    </motion.div>
+                    <motion.div
+                        className="flex flex-col gap-1"
+                        initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
+                        whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+                        transition={{ duration: animationDuration, ease: animationEase, delay: animationDelay * 3 }}
+                        viewport={{ once: true, amount: 0.2 }}
+                    >
                         <p className="text-lg md:text-xl">Email</p>
                         <input
                             className="border sm:border-2 border-black rounded-sm p-2"
@@ -53,8 +87,14 @@ export default function Contact() {
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                         />
-                    </div>
-                    <div className="flex flex-col gap-1">
+                    </motion.div>
+                    <motion.div
+                        className="flex flex-col gap-1"
+                        initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
+                        whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+                        transition={{ duration: animationDuration, ease: animationEase, delay: animationDelay * 4 }}
+                        viewport={{ once: true, amount: 0.2 }}
+                    >
                         <p className="text-lg md:text-xl">Message</p>
                         <textarea
                             className="border sm:border-2 border-black rounded-sm p-2"
@@ -63,35 +103,54 @@ export default function Contact() {
                             value={message}
                             onChange={(e) => setMessage(e.target.value)}
                         />
-                    </div>
-                    <button
+                    </motion.div>
+                    <motion.button
                         className="w-full border sm:border-2 border-black rounded-sm p-4 mt-4 cursor-pointer"
                         type="submit"
+                        initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
+                        whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+                        transition={{ duration: animationDuration, ease: animationEase, delay: animationDelay * 5 }}
+                        viewport={{ once: true, amount: 0.2 }}
                     >
                         Send Message
-                    </button>
+                    </motion.button>
                 </form>
             </div>
             {/* Right side */}
             <div className="flex flex-row lg:flex-col max-lg:justify-center gap-4 text-md lg:text-lg">
-                <a href={`mailto:${contactLinks.email}`} className="flex flex-row gap-3 cursor-pointer">
+                <motion.a
+                    href={`mailto:${contactLinks.email}`}
+                    className="flex flex-row gap-3 cursor-pointer"
+                    initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
+                    whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+                    transition={{ duration: animationDuration, ease: animationEase, delay: animationDelay * 2 }}
+                    viewport={{ once: true, amount: 0.2 }}
+                >
                     <img src={mailIcon} alt="mail" />
                     <p className="text-blackText hover:text-blackText visited:text-blackText font-normal">Email</p>
-                </a>
-                <button
+                </motion.a>
+                <motion.button
                     onClick={() => window.open(contactLinks.github, '_blank')}
                     className="flex flex-row gap-3 cursor-pointer appearance-none"
+                    initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
+                    whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+                    transition={{ duration: animationDuration, ease: animationEase, delay: animationDelay * 2.25 }}
+                    viewport={{ once: true, amount: 0.2 }}
                 >
                     <img src={githubIcon} alt="github" />
                     <p>GitHub</p>
-                </button>
-                <button
+                </motion.button>
+                <motion.button
                     onClick={() => window.open(contactLinks.linkedIn, '_blank')}
                     className="flex flex-row gap-3 cursor-pointer"
+                    initial={{ opacity: 0, y: animationYStart, x: animationXStart, scale: 0.98 }}
+                    whileInView={{ opacity: 1, y: 0, x: 0, scale: 1 }}
+                    transition={{ duration: animationDuration, ease: animationEase, delay: animationDelay * 2.5 }}
+                    viewport={{ once: true, amount: 0.2 }}
                 >
                     <img src={linkedinIcon} alt="linkedin" />
                     <p>LinkedIn</p>
-                </button>
+                </motion.button>
             </div>
         </section>
     );
