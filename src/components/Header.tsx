d2a4c523dea21d109a61cfcfb30c6e2818e25d1a
Alejandro Lopez <lopez.alejandro@dali.dartmouth.edu>
2025-11-27 11:25:16 -0500

    typo in logo import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import logo from '../assets/Logo.svg';
+import logo from '../assets/logo.svg';
 import menu from '../assets/menu.svg';
 import { scrollToSection } from '../utils/constants';
 
